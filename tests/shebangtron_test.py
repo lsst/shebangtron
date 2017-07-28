53d843e5b5cf2ada2b9347abe6475c26f07b7581
Joshua Hoblitt <josh@hoblitt.com>
2017-07-28 14:35:48 -0700

    add basic pytest execution of shebangtron

---
+++ tests/shebangtron_test.py
@@ -0,0 +1,34 @@
+import sys
+from subprocess import check_output
+
+def test_not_py_shebang(tmpdir, monkeypatch):
+    flavor = "deadbeef"
+    python = "snake"
+
+    flavor_dir = tmpdir.mkdir(flavor)
+    test_file = flavor_dir.join("mangleme")
+    test_file.write("#!/bin/sh")
+
+    monkeypatch.setenv("EUPS_PATH", tmpdir)
+    monkeypatch.setenv("SHTRON_EUPS_FLAVOR", flavor)
+    monkeypatch.setenv("SHTRON_PYTHON", python)
+
+    print(check_output(["./shebangtron"]))
+
+    assert test_file.read() == "#!/bin/sh"
+
+def test_vanilla_py_shebang(tmpdir, monkeypatch):
+    flavor = "deadbeef"
+    python = "snake"
+
+    flavor_dir = tmpdir.mkdir(flavor)
+    test_file = flavor_dir.join("mangleme")
+    test_file.write("#!python")
+
+    monkeypatch.setenv("EUPS_PATH", tmpdir)
+    monkeypatch.setenv("SHTRON_EUPS_FLAVOR", flavor)
+    monkeypatch.setenv("SHTRON_PYTHON", python)
+
+    print(check_output(["./shebangtron"]))
+
+    assert test_file.read() == "#!" + python
